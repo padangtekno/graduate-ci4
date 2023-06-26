6d1284adafc0832ac1dbc43302a6b23a3f482659
padangtekno <padang.tutorial@gmail.com>
2023-06-27 00:10:24 +0700

    Padang Tekno

--- app/Views/mahasiswa/v_dashboard.php
+++ app/Views/mahasiswa/v_dashboard.php
@@ -1,3 +1,31 @@
+<?php
+
+$db = \Config\Database::connect();
+
+
+$dokumen = $db->table('tbl_dokumen')
+    ->where('npm', session()->get('npm'))
+    ->get()->getResultArray();
+
+if ($dokumen <> null) {
+    foreach ($dokumen as $key => $value) {
+        $id_syarat[] = $value['id_persyaratan'];
+    }
+} else {
+    $id_syarat[] = 0;
+}
+$syarat = $db->table('tbl_persyaratan')
+    ->whereNotIn('tbl_persyaratan.id_persyaratan', $id_syarat)
+    ->get()->getResultArray();
+
+///=============
+$dokumen2 = $db->table('tbl_dokumen')
+    ->join('tbl_persyaratan', 'tbl_persyaratan.id_persyaratan=tbl_dokumen.id_persyaratan')
+    ->where('tbl_dokumen.npm', session()->get('npm'))
+    ->get()->getResultArray();
+
+?>
+
 <div class="col-sm-12">
     <div class="callout callout-info">
         <h5>Selamat Datang <b class="text-primary"><?= $mhs['nama_mahasiswa'] ?></b></h5>
@@ -7,13 +35,18 @@
 <div class="col-sm-12">
     <h3><b>Persyaratan Wisuda</b></h3>
     <div class="row">
-        <?php foreach ($persyaratan as $key => $value) { ?>
+        <?php foreach ($syarat as $key => $value) { ?>
             <div class="col-sm-6">
-
-                <div class="callout callout-success">
-                    <h5><i class="icon fas fa-check"></i> <?= $value['nama_persyaratan'] ?></h5>
+                <div class="callout callout-danger">
+                    <h5><i class="icon fas fa-times text-danger"></i> <?= $value['nama_persyaratan'] ?></h5>
                 </div>
-
+            </div>
+        <?php } ?>
+        <?php foreach ($dokumen2 as $key => $value) { ?>
+            <div class="col-sm-6">
+                <div class="callout callout-success">
+                    <h5><i class="icon fas fa-check text-success"></i> <?= $value['nama_persyaratan'] ?></h5>
+                </div>
             </div>
         <?php } ?>
     </div>
